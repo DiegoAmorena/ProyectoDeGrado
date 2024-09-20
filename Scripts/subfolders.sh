4025ffe5948ff3267e18066c0b8e990cb1907c3c
Diego Amorena <diegoamorenag@gmail.com>
2024-09-19 22:06:14 -0300

    asdasd

--- Scripts/subfolders.sh
+++ Scripts/subfolders.sh
@@ -1,31 +1,22 @@
 #!/bin/bash
 
-# Check if the correct number of arguments are provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <source_directory>"
-    exit 1
-fi
+SOURCE_DIR="$1"
 
-# Assign the argument to a variable
-SOURCE_DIR=$1
-
-# Define the target directories
 TARGET_DIRS=("medium" "small" "tiny" "base" "large")
 
 # Create target directories if they don't exist
 for DIR in "${TARGET_DIRS[@]}"; do
-    mkdir -p "$SOURCE_DIR/$DIR"
+    TARGET_PATH="$SOURCE_DIR/$DIR"
+    if [ ! -d "$TARGET_PATH" ]; then
+        mkdir -p "$TARGET_PATH"
+    fi
 done
 
 # Move files to their corresponding directories
 for TARGET in "${TARGET_DIRS[@]}"; do
-    # Find files that end with the target keyword and move them
-    for FILE in "$SOURCE_DIR"/*$TARGET; do
-        if [[ -f "$FILE" ]]; then
-            # Move the file to the target directory
-            mv "$FILE" "$SOURCE_DIR/$TARGET/"
-        fi
+    FILES=$(find "$SOURCE_DIR" -maxdepth 1 -type f -name "*$TARGET*")
+    for FILE in $FILES; do
+        DEST_PATH="$SOURCE_DIR/$TARGET"
+        mv "$FILE" "$DEST_PATH"
     done
 done
-
-echo "Files have been moved."
